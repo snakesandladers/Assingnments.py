dbf59a3a98ebee9e2314e9e62a1a5d16eaf11681
snakesandladers <93540215+snakesandladers@users.noreply.github.com>
2021-12-17 13:22:24 +0530

    Add files via upload

---
+++ Assingnments.py
@@ -0,0 +1,19 @@
+x=10
+y=5
+
+#not equal to operator
+x!=y
+print (x!=y)
+#greater than operator
+x>y
+print(x>y)
+#less than operator 
+x<y
+print(x<y)
+#greater than or equal to operator 
+x>=y
+print(x>=y)
+#less than or equal to operator 
+x<=y
+print(x<=y)
+
