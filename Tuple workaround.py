834d5a19bc15533bdc5d8141b0bd2c3d0d48b4ac
snakesandladers <93540215+snakesandladers@users.noreply.github.com>
2021-12-31 11:53:14 +0530

    Update Tuple workaround.py

--- Tuple workaround.py
+++ Tuple workaround.py
@@ -5,7 +5,8 @@ y[1] = "kiwi"
 x = tuple(y)
 
 print(x) 
-
+Output:
+  ('apple', 'kiwi', 'cherry')
 
 
 #Dictionary
@@ -13,3 +14,5 @@ l1=['a','b','c','d','e']
 l2=[1,2,3,4,5]
 d=dict(zip(l1,l2))
 print(d)
+Output:
+  {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
