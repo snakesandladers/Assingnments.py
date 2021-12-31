27a63160334571c54d74e5c79d3e757a8ca5b18d
snakesandladers <93540215+snakesandladers@users.noreply.github.com>
2021-12-31 11:05:47 +0530

    Add files via upload

---
+++ Dictionary method.py
@@ -0,0 +1,89 @@
+#clear
+car={
+    "car":"ford",
+    "model":"Mustang",
+    "year":1964
+    }
+car.clear()
+print(car)
+#Copy
+car={
+    "car":"ford",
+    "model":"Mustang",
+    "year":1964
+    }
+x=car.copy()
+print(x)
+#fromkeys
+x=('key 1','key 2','key 3')
+y=0
+thisdict=dict.fromkeys(x,y)
+print(thisdict)
+#get
+car={
+    "car":"ford",
+    "model":"Mustang",
+    "year":1964
+    }
+x=car.get("model")
+print(x)
+#Items
+car={
+    "car":"ford",
+    "model":"Mustang",
+    "year":1964
+    }
+x=car.items()
+print(x)
+#keys
+car={
+    "car":"ford",
+    "model":"Mustang",
+    "year":1964
+    }
+x=car.keys()
+print(x)
+#Pop
+car={
+    "car":"ford",
+    "model":"Mustang",
+    "year":1964
+    }
+car.pop("model")
+print(car)
+#Pop item
+car={
+    "car":"ford",
+    "model":"Mustang",
+    "year":1964
+    }
+car.popitem()
+print(car)
+#default
+car={
+    "car":"ford",
+    "model":"Mustang",
+    "year":1964
+    }
+x=car.setdefault("model","Bronco")
+print(x)
+#update
+car={
+    "car":"ford",
+    "model":"Mustang",
+    "year":1964
+    }
+car.update({"color":"white"})
+print(car)
+#values
+car={
+    "car":"ford",
+    "model":"Mustang",
+    "year":1964
+    }
+x=car.values()
+print(x)
+    
+
+
+
